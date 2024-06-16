4490ad32530e77341018dd3bd0fff7e7f0713752
ponnanapavan <pavanponnana1@gmail.com>
2024-06-16 17:07:50 +0530

    change

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,33 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Heart, HeartPulse, Search, Soup } from 'lucide-react'
-const RecipeCard = () => {
+const getValues=(arr)=>{
+  return [arr[0],arr[1]];
+}
+const RecipeCard = ({recipe}) => {
+  const [favorite, setFavorite]=useState(localStorage.getItem('favorites')?.includes(recipe.label))
+ const addRecipes=()=>{
+  let favorites=JSON.parse(localStorage.getItem('favorites'))||[];
+  const isAlreadyFavorites=favorites.some((fav)=>fav.label === recipe.label)
+  if(isAlreadyFavorites){
+    favorites=favorites.filter((fav)=>fav.label !== recipe.label);
+    setFavorite(false);
+  }else{
+    favorites.push(recipe);
+    setFavorite(true)
+  }
+     localStorage.setItem('favorites', JSON.stringify(favorites))
+
+
+ }
+
   return (
     <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative'>
-    <a href="#" className='relative h-40'>
-        <img src="./1.jpg" alt=""  className='rounded-md w-full h-full object-cover cursor-pointer'/>
+    <a href={`https://www.youtube.com/results?search_query=${recipe.label} recipe`} target='_blank' className='relative h-40'>
+        <img src={recipe?.image} alt=""  className='rounded-md w-full h-full object-cover cursor-pointer'/>
         <div className='absolute bottom-6 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm'>
-            <Soup size={20}/> 2 servings
+            <Soup size={20}/> {recipe?.yield} servings
         </div>
-        <div className='absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer'>
-            <Heart size={25} className='hover:fill-red-500 hover:text-red-500'/>
+        <div className='absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer' 
+        onClick={(e)=>{
+          e.preventDefault();
+          addRecipes();
+
+        }}
+        >
+           {!favorite && <Heart size={25} className='fill-white '/>}
+           {favorite &&  <Heart size={25} className='fill-red-500 text-red-500'/>}
+           
         </div>
      </a>
      <div className='flex mt-1'>
-        <p className='font-bold tracking-wide'>Roasted Chicken</p>
+        <p className='font-bold tracking-wide'>{recipe?.label}</p>
      </div>
-     <p className='my-2'>Turkish Chicken</p>
+     <p className='my-2'>{recipe?.cuisineType[0].charAt(0).toUpperCase()+ recipe.cuisineType[0].slice(1)}</p>
 
      <div className='flex gap-4 mt-auto'>
-         <div className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md'>
+         {getValues(recipe.healthLabels).map((label,index)=>(
+           <div key={index} className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md'>
             <HeartPulse size={16}/>
-            <span className='text-sm tracking-tighter font-semibold'>Gluten-free</span>
-
-         </div>
-         <div className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md'>
-            <HeartPulse size={16}/>
-            <span className='text-sm tracking-tighter font-semibold'>Gluten-free</span>
-
-         </div>
+            <span className='text-sm tracking-tighter font-semibold'>{label}</span>
+           </div>
+         ))}
      </div>
     </div>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,72 @@
 import { Heart, HeartPulse, Search, Soup } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import RecipeCard from '../components/RecipeCard'
-
+const APP_ID='bb43607c'
+  const APP_KEY='db0bc51a13a77a915083987284ba5933'
 const HomePage = () => {
+  const [Recipes, setRecipes]=useState([]);
+  const [loading, setLoading]=useState(true);
+
+  function handlesearch(e){
+    e.preventDefault();
+    fetchRecipes(e.target.value);
+
+  }
+  const fetchRecipes=async(searchQuery)=>{
+    setLoading(true)
+    setRecipes([])
+    try{
+      const response=await fetch(
+        `https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`)
+        const data=await response.json();
+        console.log(data);
+       setRecipes(data.hits);
+    }catch(err){
+      console.log(err.message)
+    }finally{
+      setLoading(false);
+    }
+
+  }
+  
+        useEffect(()=>{
+          fetchRecipes();
+
+        },[])
   return (
     <div className='bg-[#faf9fb] p-10 flex-1'>
         <div className='max-w-screen-lg mx-auto'>
             <form >
                 <label className='input shadow-md flex items-center gap-2'>
                     <Search size={"24"}/>
-                    <input type="text" className='text-sm md:text-md grow' placeholder='what do you want to cook today'/>
+                    <input type="text" className='text-sm md:text-md grow' placeholder='what do you want to cook today' onChange={handlesearch} 
+                    />
                 </label>
             </form>
             <h1 className='font-bold text-3xl md:text-5xl mt-4'>Recommanded Recipes</h1>
             <p className='text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight'>popular choices</p>
            <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
-          <RecipeCard/>
+            {!loading && Recipes?.map(({recipe},index)=>(
+              <RecipeCard key={index} recipe={recipe}/>
+            ))}
+
+
+
+
+
+             {loading && 
+               [...Array(9)].map((_,index)=>(
+                   <div key={index} className='flex flex-col gap-5 w-full'>
+                    <div className='skeleton h-32 w-full'></div>
+                    <div className='flex justify-between'>
+                      <div className='skeleton h-4 w-28'></div>
+                      <div className='skeleton h-4 w-28'></div>
+                    </div>
+                    <div className='skeleton h-4 w-1/2'></div>
+                   </div>
+               ))
+             
+             }
             
            </div>
         </div>
